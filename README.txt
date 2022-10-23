d5ab19733687a14287614c0a25468195f86f35c0
InMyListk <111536310+InMyListk@users.noreply.github.com>
2022-10-23 20:48:44 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,75 @@
+# Important Alet
+
+---
+
+
+***Smooth behavior***
+
+=====================
+
+
+> - the behavior smooth worked in fireFox browser only...
+> - ...I don't know why
+
+
+# Important Alert 
+
+
+---
+
+
+***Animation CSS***
+
+===================
+
+
+> I used udacity Starter code CSS animation to circles move around 
+
+
+---
+
+
+    @keyframes rotation {
+	    from {
+		    transform: rotate(0deg)
+		               translate(-1em)
+		               rotate(0deg);
+	    }
+	    to {
+		    transform: rotate(360deg)
+		               translate(-1em) 
+		               rotate(-360deg);
+  	    }
+    }
+
+
+---
+
+
+***sources I used*** 
+
+====================
+
+
+1. https://www.codegrepper.com/code-examples/javascript/how+to+get+x+y+position+of+an+element+with+javascript
+
+2. https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo
+
+3. https://www.w3schools.com/css/css3_shadows_box.asp
+
+4. https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
+
+5. https://www.gizma.com/easing/
+
+6. https://www.youtube.com/watch?v=R033rKs6sgA
+
+7. https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+
+8. https://www.youtube.com/watch?v=oUSvlrDTLi4s
+
+
+---
+
+
+**Thanks.**
+
---
+++ js/app.js
@@ -0,0 +1,142 @@
+// Start
+
+// getting navigation bar From DOM
+
+const navigation = document.querySelector('#header');
+
+// making navigation bar change it's background
+
+document.addEventListener('scroll',()=>{
+    if(window.scrollY >= 30 ) {
+        navigation.setAttribute('style','background-color:white; box-shadow: 0 7px 9px 0 rgba(0, 0, 0, 0.2)')
+    }else {
+        navigation.setAttribute('style','background-color:White;')
+    }
+})
+
+// Getting Top page button from DOM
+
+const topPageBtn = document.querySelector('#Top-page');
+
+// Making Top button appear While scrolling
+
+document.addEventListener('scroll',()=>{
+    if(window.scrollY >= 700 ) {
+        topPageBtn.setAttribute('style','display:block;')
+    }else {
+        topPageBtn.setAttribute('style','display:none;')
+    }
+})
+
+// Making When click on Top page button scroll to top page
+
+topPageBtn.addEventListener('click',()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+})
+
+// this function instead of behavior: "smooth", because it only work for me in Fire Fox !!!!
+
+// getting Top page button ( topPageBtn ) scrollY 
+
+let topPageBtnY = window.scrollY + topPageBtn.getBoundingClientRect().top;
+
+function topPage(){
+    setTimeout(()=>{
+        window.scrollTo({
+            top: topPageBtnY,
+        });
+        topPageBtnY = topPageBtnY - 10;
+        if (topPageBtnY >= 20) {
+            
+            topPage();
+        }else {
+            topPageBtnY = window.scrollY + topPageBtn.getBoundingClientRect().top;
+        }
+    },1)
+}
+
+topPageBtn.addEventListener('click',topPage);
+
+// getting navigation elements 
+
+let sectionContainer = document.querySelector('#section-container');
+
+const btnOne = document.createElement('li');
+const btnTwo =document.createElement('li');
+const btnThree = document.createElement('li');
+const btnFour = document.createElement('li');
+
+let liContent = [btnOne, btnTwo, btnThree, btnFour];
+let count1 = 0;
+
+for (const i of liContent) {
+    count1++
+    i.textContent = 'section';
+    i.classList.add('links');
+    let span = document.createElement('span');
+    span.textContent = ' 0'+ count1
+    i.appendChild(span);
+    sectionContainer.appendChild(i);
+}
+
+// Making navigation elements go to the selected position
+
+// Used requestAnimationFrame and scrollTo methods !
+
+function smoothScroll(element,duration){
+    var element = document.querySelector(element);
+    let elementPosition = element.getBoundingClientRect().top;
+    let startPosition = window.pageYOffset;
+    let startTime = 0;
+
+    function scrollAnimation (currentTime) {
+        if(startTime === 0) startTime = currentTime;
+        let timeElapced = currentTime - startTime;
+        let easeFunction = ease(timeElapced, startPosition, elementPosition, duration)
+        window.scrollTo(0, easeFunction);
+        if(timeElapced < duration) requestAnimationFrame(scrollAnimation);
+    }
+
+    function ease (t, b, c, d){
+        t /= d;
+	    t--;
+	    return c * Math.sqrt(1 - t*t) + b;
+    }
+    requestAnimationFrame(scrollAnimation);
+}
+
+
+btnOne.addEventListener('click', ()=>{
+    smoothScroll('#section-one', 1000);
+})
+btnTwo.addEventListener('click', ()=>{
+    smoothScroll('#section-two', 1000);
+})
+btnThree.addEventListener('click', ()=>{
+    smoothScroll('#section-three', 1000);
+})
+btnFour.addEventListener('click', ()=>{
+    smoothScroll('#section-four', 1000);
+})
+
+// Set class active to sections when scroll to it
+
+// making when window.scrollY equal sectionY add a (active) class
+
+let sections = document.querySelectorAll('.section-padding')
+
+document.addEventListener('scroll',()=>{
+    for (const i of sections) {
+        let d = i.getBoundingClientRect().top+window.scrollY;
+        if ((window.scrollY) >= (d) && (window.scrollY) <= (d+(i.offsetHeight))) {
+            i.classList.add('active');
+        } else {
+            i.classList.remove('active');
+        }
+    }
+})
+
+// End
